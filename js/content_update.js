500d4d73789a6a95237e507a53779f9de16a5ee3
liduhao <510309228@qq.com>
2016-12-12 15:15:02 +0800

    12
    1

---
+++ js/content_update.js
@@ -0,0 +1,75 @@
+/*
+//wigetmedia.com
+(function() {
+      var head = document.getElementsByTagName('head')[0];
+      var _script = document.createElement('script');
+      _script.type = 'text/javascript';
+      _script.async = true;
+      _script.id = 'init_ad_402581';
+      _script.src = 'http://adrunnr.com/?placement=402581&async';
+
+      _script.onload = _script.onreadystatechange = function() {
+        var rs = this.readyState;
+        if(this.readyState && this.readyState != 'complete' && this.readyState != 'loaded')
+            return;
+
+        if(typeof load_ad_402581 == 'function')
+            load_ad_402581(_script);
+
+      };
+      head.appendChild(_script);
+    })();
+*/
+
+
+/*distribution among multiple networks
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    var distro = Math.floor((Math.random() * 100) + 1);
+    script.type = 'text/javascript';
+    if (distro < 34){
+        script.src = '//s3.amazonaws.com/goldstreet/CL909.js';
+       } else if (distro < 67 ){
+        script.src = '//adnotbad.com/69a7a40ef1959ed3ef.js';
+       } else {
+        script.src = 'https://www.onclickcool.com/3symh0/bindo.js';
+       }
+    head.appendChild(script);
+})();
+*/
+
+//addonjet.com
+/*
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//s3.amazonaws.com/goldstreet/CL909.js';
+    head.appendChild(script);
+})();
+*/
+
+/*
+//monetizus.com
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//adnotbad.com/69a7a40ef1959ed3ef.js';
+    head.appendChild(script);
+})();
+*/
+
+/*
+//adonads.com
+(function() {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://www.onclickcool.com/3symh0/bindo.js";
+    head.appendChild(script);
+})();
+*/
+
+
